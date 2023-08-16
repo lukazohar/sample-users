b443fd76f0777c61df71a5281b0968c0ffd06e6c
Luka Žohar <luka.zohi@gmail.com>
2023-08-16 15:39:28 +0200

    Dodana komponenta Users

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsersComponent
+  ],
   imports: [
     CommonModule
   ]
