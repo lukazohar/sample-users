c25a774e3fb8915d7f8cb264bb5a93984e2c7241
Luka Žohar <luka.zohi@gmail.com>
2023-08-16 17:07:01 +0200

    Import HTTP modula in dobivanje podatkov v users komponenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersModule } from './modules/users/users.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { UsersModule } from './modules/users/users.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UsersModule
+    UsersModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IUser } from './user.interface';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-users',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+  users: Array<IUser> = [];
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get<Array<IUser>>("http://localhost:3000/users").subscribe(
+      users => this.users = users,
+      err => console.log(err)
+    );
+  }
+
+  deleteUser(id: number, index: number) {
+    this.http.delete<any>(`http://localhost:3000/users/${id}`).subscribe(
+      _ => this.users.splice(index, 1),
+      err => console.log(err)
+    );
+  }
 }
